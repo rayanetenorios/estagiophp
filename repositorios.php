27afdfef54b9f03e19cd10751aad215d88bb0402
Rayane Tenorio <rayanemtenorio@gmail.com>
2021-05-21 16:15:38 -0300

    limpeza do código

--- repositorios.php
+++ repositorios.php
@@ -4,33 +4,34 @@
     <head>
         <meta charset="utf-8">
         <link href= "style.css" rel= "stylesheet">
-        <title>Resultado da Pesquisa</title>
+        <title>Repositórios do Desenvolvedor</title>
     </head>
 
 <body>
     <?php
 
-    $pesquisa= $_POST['pesquisa'];
-        echo 'Resultado da Pesquisa: '.$pesquisa.'</br>';
+    $usuario= $_POST['login'];
+        echo 'Repositórios de: '. $usuario .'</br>';
 
     $ch = curl_init(); //inicia a curl
-    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/search/users?q=".$pesquisa);
+    curl_setopt($ch, CURLOPT_URL, "https://api.github.com/users/". $usuario ."/repos");
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         "Accept: application/vnd.github.v3+json",
         "Content-Type: text/plain",
         "User-Agent: Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 YaBrowser/16.3.0.7146 Yowser/2.5 Safari/537.36"]);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-    $response = json_decode(curl_exec($ch)); //exibe organizado
+    $repositorios = json_decode(curl_exec($ch)); //exibe organizado
     curl_close($ch); //finaliza a curl
 
-    //var_dump ($response); //exibição primária
+    var_dump ($repositorios); //exibição primária
 
-    foreach ($response->items as $usuario) {
-        echo "</br>Nome: " . "<a href= 'repositorios.php'>" . $usuario->login . "</a>";
-        echo "</br>GitHub: " . $usuario->html_url . "</br>";
+    /*foreach ($repositorios as $repos_user) {
+        echo "</br>Nome do Repositório: " . $repos_user->name;
+        echo "</br>Endereço no GitHub: " . $repos_user->html_url;
+        echo "</br>Linguagem: " . $repos_user->language . "</br>";
     };
-
+*/
     ?>
 </body>
 
